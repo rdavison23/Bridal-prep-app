e1ca975f01fb5ae6735be85b239ff2334c9e94e9
Your Name <your-email@example.com>
2026-04-23 13:53:42 -0700

    update express server to use routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import budgetRoutes from './routes/budgetRoutes';
+import checklistRoutes from './routes/checklistRoutes';
+import quizRoutes from './routes/quizRoutes';
+
 dotenv.config();
 
 const app = express();
@@ -11,6 +15,11 @@ app.use(express.json());
 app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
+
+//API routes
+app.use('/api/budget', budgetRoutes);
+app.use('api/checklist', checklistRoutes);
+app.use('/api/quiz', quizRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
