a5dcaa0d6efaf9522f056b98376e4913b0199680
Your Name <your-email@example.com>
2026-04-28 17:29:41 -0700

    fix(quiz) rename results component and correct result field names

--- client/src/components/quiz/QuizResults.jsx
+++ client/src/components/quiz/QuizResults.jsx
@@ -1,14 +1,15 @@
 import { useLocation } from 'react-router-dom';
 
-export default function QuizQuestion() {
+export default function QuizResults() {
   const { state } = useLocation();
 
-  if (!state) return <p> No result found</p>;
+  if (!state) return <p>No results found.</p>;
 
-  const { style_profile, images, quiz_verstion } = state;
+  const { style_profile, images, quiz_version } = state;
 
   return (
-    <div className="quiz_results">
+    <div className="quiz-results">
+      <h1>Your Style: {style_profile}</h1>
       <p>Quiz Version: {quiz_version}</p>
 
       <div className="results-images">
