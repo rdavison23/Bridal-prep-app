c8e14d01639993353d27683d4b32a0416c5910f1
Your Name <your-email@example.com>
2026-05-06 17:43:46 -0700

    Change db client to look for bridal_prep

--- server/db.js
+++ server/db.js
@@ -7,8 +7,11 @@ const { Pool } = pkg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  options: '-c search_path=bridal_prep',
+  ssl: { rejectUnauthorized: false },
 });
+
+// Force the correct schema for ALL queries
+pool
+  .query('SET search_path TO bridal_prep, public;')
+  .then(() => console.log('Search path set to bridal_prep'))
+  .catch((err) => console.error('Error setting search path:', err));
