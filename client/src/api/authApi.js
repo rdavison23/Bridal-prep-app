3f4ce6c7a2d0d964b22f933636185864c1fcb1cd
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-06-06 17:44:47 -0400

    Add authApi skeleton for authentication

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,27 +1,27 @@
 import apiClient from './apiClient';
 
-// POST /api/signup 
+// POST /api/signup
 export async function signupUser(data) {
-    try {
-        return apiClient('/api/auth/signup', {
-            method: 'POST',
-            body: JSON.stringify(data),
-        });
-    } catch (err) {
-        console.error('Failed to sign up:', err);
-        throw new Error('Unable to signup. Please try again.');
-    }
+  try {
+    return apiClient('/api/auth/signup', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    });
+  } catch (err) {
+    console.error('Failed to sign up:', err);
+    throw new Error('Unable to signup. Please try again.');
+  }
 }
 
-// POST /api/login 
+// POST /api/login
 export async function loginUser(data) {
-    try {
-        return apiClient('/api/auth/login', {
-            method: 'POST',
-            body: JSON.stringify(data),
-        });
-    } catch (err) {
-        console.error('Failed to Log in:', err);
-        throw new Error('Unable to Log in. Please try again.');
-    }
+  try {
+    return apiClient('/api/auth/login', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    });
+  } catch (err) {
+    console.error('Failed to Log in:', err);
+    throw new Error('Unable to Log in. Please try again.');
+  }
 }
