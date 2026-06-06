2b21f5cb44f40c670a18f3a93a2ccd273e391e98
Shu Wang <swang3130@gatech.edu>
2026-06-06 18:21:51 -0400

    add save token to localStorage after Signup successfully

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -2,9 +2,12 @@ import AuthForm from "../components/auth/AuthForm";
 import { signupUser } from "../api/authApi";
 const SignUpPage = () => {
     const handleSignup = async (formData) => {
-        console.log(formData);
-
-        await signupUser(formData);
+        try {
+            const result = await signupUser(formData);
+            localStorage.setItem("token", result.token);
+        } catch (err) {
+            console.error(err);
+        }
     }
 
     return (
