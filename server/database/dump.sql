c7bb9196679a8d0f8037b9fdd405ff80fff6b08b
Your Name <your-email@example.com>
2026-06-06 19:28:06 -0700

    update dump with name and role colums

--- server/database/dump.sql
+++ server/database/dump.sql
@@ -2,10 +2,10 @@
 -- PostgreSQL database dump
 --
 
-\restrict bTrkLoLdKCnjvfGgQnMOfACUTipAgOkHbOUfl4QHtnLiEJ20tCCdx0MYPCOoK2a
+\restrict kIW3zqcWTcoNw4f75cyYrAgM2sM7Knn6cf9nefVfXfoJXm3lfIQffvqZrfurWgo
 
--- Dumped from database version 18.1 (Homebrew)
--- Dumped by pg_dump version 18.1 (Homebrew)
+-- Dumped from database version 18.3 (Homebrew)
+-- Dumped by pg_dump version 18.3 (Homebrew)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -19,41 +19,29 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- Name: bridal_prep; Type: SCHEMA; Schema: -; Owner: redu
---
-
-CREATE SCHEMA bridal_prep;
-
-
-ALTER SCHEMA bridal_prep OWNER TO redu;
-
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
 --
--- Name: budgets; Type: TABLE; Schema: bridal_prep; Owner: redu
+-- Name: books; Type: TABLE; Schema: public; Owner: redu
 --
 
-CREATE TABLE bridal_prep.budgets (
+CREATE TABLE public.books (
     id integer NOT NULL,
-    user_id integer,
-    ideal_budget integer NOT NULL,
-    max_budget integer NOT NULL,
-    hidden_costs_total integer NOT NULL,
-    final_estimate integer NOT NULL,
-    created_at timestamp without time zone DEFAULT now()
+    title text NOT NULL,
+    author text NOT NULL,
+    year integer
 );
 
 
-ALTER TABLE bridal_prep.budgets OWNER TO redu;
+ALTER TABLE public.books OWNER TO redu;
 
 --
--- Name: budgets_id_seq; Type: SEQUENCE; Schema: bridal_prep; Owner: redu
+-- Name: books_id_seq; Type: SEQUENCE; Schema: public; Owner: redu
 --
 
-CREATE SEQUENCE bridal_prep.budgets_id_seq
+CREATE SEQUENCE public.books_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -62,35 +50,35 @@ CREATE SEQUENCE bridal_prep.budgets_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE bridal_prep.budgets_id_seq OWNER TO redu;
+ALTER SEQUENCE public.books_id_seq OWNER TO redu;
 
 --
--- Name: budgets_id_seq; Type: SEQUENCE OWNED BY; Schema: bridal_prep; Owner: redu
+-- Name: books_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: redu
 --
 
-ALTER SEQUENCE bridal_prep.budgets_id_seq OWNED BY bridal_prep.budgets.id;
+ALTER SEQUENCE public.books_id_seq OWNED BY public.books.id;
 
 
 --
--- Name: checklist_items; Type: TABLE; Schema: bridal_prep; Owner: redu
+-- Name: events; Type: TABLE; Schema: public; Owner: redu
 --
 
-CREATE TABLE bridal_prep.checklist_items (
+CREATE TABLE public.events (
     id integer NOT NULL,
-    user_id integer,
-    item_text text NOT NULL,
-    is_completed boolean DEFAULT false,
-    created_at timestamp without time zone DEFAULT now()
+    title text NOT NULL,
+    date date NOT NULL,
+    description text,
+    is_favorite boolean DEFAULT false
 );
 
 
-ALTER TABLE bridal_prep.checklist_items OWNER TO redu;
+ALTER TABLE public.events OWNER TO redu;
 
 --
--- Name: checklist_items_id_seq; Type: SEQUENCE; Schema: bridal_prep; Owner: redu
+-- Name: events_id_seq; Type: SEQUENCE; Schema: public; Owner: redu
 --
 
-CREATE SEQUENCE bridal_prep.checklist_items_id_seq
+CREATE SEQUENCE public.events_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -99,244 +87,83 @@ CREATE SEQUENCE bridal_prep.checklist_items_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE bridal_prep.checklist_items_id_seq OWNER TO redu;
+ALTER SEQUENCE public.events_id_seq OWNER TO redu;
 
 --
--- Name: checklist_items_id_seq; Type: SEQUENCE OWNED BY; Schema: bridal_prep; Owner: redu
+-- Name: events_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: redu
 --
 
-ALTER SEQUENCE bridal_prep.checklist_items_id_seq OWNED BY bridal_prep.checklist_items.id;
-
-
---
--- Name: quiz_results; Type: TABLE; Schema: bridal_prep; Owner: redu
---
-
-CREATE TABLE bridal_prep.quiz_results (
-    id integer NOT NULL,
-    user_id integer,
-    style_profile text NOT NULL,
-    image_urls text[],
-    quiz_version integer DEFAULT 1 NOT NULL,
-    created_at timestamp without time zone DEFAULT now()
-);
-
-
-ALTER TABLE bridal_prep.quiz_results OWNER TO redu;
-
---
--- Name: quiz_results_id_seq; Type: SEQUENCE; Schema: bridal_prep; Owner: redu
---
-
-CREATE SEQUENCE bridal_prep.quiz_results_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER SEQUENCE bridal_prep.quiz_results_id_seq OWNER TO redu;
-
---
--- Name: quiz_results_id_seq; Type: SEQUENCE OWNED BY; Schema: bridal_prep; Owner: redu
---
-
-ALTER SEQUENCE bridal_prep.quiz_results_id_seq OWNED BY bridal_prep.quiz_results.id;
+ALTER SEQUENCE public.events_id_seq OWNED BY public.events.id;
 
 
 --
--- Name: users; Type: TABLE; Schema: bridal_prep; Owner: redu
+-- Name: books id; Type: DEFAULT; Schema: public; Owner: redu
 --
 
-CREATE TABLE bridal_prep.users (
-    id integer NOT NULL,
-    email character varying(255) NOT NULL,
-    password_hash text NOT NULL,
-    created_at timestamp without time zone DEFAULT now()
-);
-
-
-ALTER TABLE bridal_prep.users OWNER TO redu;
-
---
--- Name: users_id_seq; Type: SEQUENCE; Schema: bridal_prep; Owner: redu
---
-
-CREATE SEQUENCE bridal_prep.users_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER SEQUENCE bridal_prep.users_id_seq OWNER TO redu;
-
---
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: bridal_prep; Owner: redu
---
-
-ALTER SEQUENCE bridal_prep.users_id_seq OWNED BY bridal_prep.users.id;
+ALTER TABLE ONLY public.books ALTER COLUMN id SET DEFAULT nextval('public.books_id_seq'::regclass);
 
 
 --
--- Name: budgets id; Type: DEFAULT; Schema: bridal_prep; Owner: redu
+-- Name: events id; Type: DEFAULT; Schema: public; Owner: redu
 --
 
-ALTER TABLE ONLY bridal_prep.budgets ALTER COLUMN id SET DEFAULT nextval('bridal_prep.budgets_id_seq'::regclass);
-
-
---
--- Name: checklist_items id; Type: DEFAULT; Schema: bridal_prep; Owner: redu
---
-
-ALTER TABLE ONLY bridal_prep.checklist_items ALTER COLUMN id SET DEFAULT nextval('bridal_prep.checklist_items_id_seq'::regclass);
+ALTER TABLE ONLY public.events ALTER COLUMN id SET DEFAULT nextval('public.events_id_seq'::regclass);
 
 
 --
--- Name: quiz_results id; Type: DEFAULT; Schema: bridal_prep; Owner: redu
+-- Data for Name: books; Type: TABLE DATA; Schema: public; Owner: redu
 --
 
-ALTER TABLE ONLY bridal_prep.quiz_results ALTER COLUMN id SET DEFAULT nextval('bridal_prep.quiz_results_id_seq'::regclass);
-
-
---
--- Name: users id; Type: DEFAULT; Schema: bridal_prep; Owner: redu
---
-
-ALTER TABLE ONLY bridal_prep.users ALTER COLUMN id SET DEFAULT nextval('bridal_prep.users_id_seq'::regclass);
-
-
---
--- Data for Name: budgets; Type: TABLE DATA; Schema: bridal_prep; Owner: redu
---
-
-COPY bridal_prep.budgets (id, user_id, ideal_budget, max_budget, hidden_costs_total, final_estimate, created_at) FROM stdin;
+COPY public.books (id, title, author, year) FROM stdin;
+1	Maki	Redu 	1994
+2	Redu 	Wolde 	2013
+3	Pam 	Noah 	2000
 \.
 
 
 --
--- Data for Name: checklist_items; Type: TABLE DATA; Schema: bridal_prep; Owner: redu
+-- Data for Name: events; Type: TABLE DATA; Schema: public; Owner: redu
 --
 
-COPY bridal_prep.checklist_items (id, user_id, item_text, is_completed, created_at) FROM stdin;
+COPY public.events (id, title, date, description, is_favorite) FROM stdin;
+1	Birthday	2026-07-23	Birthday celebration!	t
+2	Easter	2026-05-05	sun, Apr 5th Easter!	f
 \.
 
 
 --
--- Data for Name: quiz_results; Type: TABLE DATA; Schema: bridal_prep; Owner: redu
+-- Name: books_id_seq; Type: SEQUENCE SET; Schema: public; Owner: redu
 --
 
-COPY bridal_prep.quiz_results (id, user_id, style_profile, image_urls, quiz_version, created_at) FROM stdin;
-\.
-
-
---
--- Data for Name: users; Type: TABLE DATA; Schema: bridal_prep; Owner: redu
---
-
-COPY bridal_prep.users (id, email, password_hash, created_at) FROM stdin;
-\.
+SELECT pg_catalog.setval('public.books_id_seq', 3, true);
 
 
 --
--- Name: budgets_id_seq; Type: SEQUENCE SET; Schema: bridal_prep; Owner: redu
+-- Name: events_id_seq; Type: SEQUENCE SET; Schema: public; Owner: redu
 --
 
-SELECT pg_catalog.setval('bridal_prep.budgets_id_seq', 1, false);
-
-
---
--- Name: checklist_items_id_seq; Type: SEQUENCE SET; Schema: bridal_prep; Owner: redu
---
-
-SELECT pg_catalog.setval('bridal_prep.checklist_items_id_seq', 1, false);
+SELECT pg_catalog.setval('public.events_id_seq', 2, true);
 
 
 --
--- Name: quiz_results_id_seq; Type: SEQUENCE SET; Schema: bridal_prep; Owner: redu
+-- Name: books books_pkey; Type: CONSTRAINT; Schema: public; Owner: redu
 --
 
-SELECT pg_catalog.setval('bridal_prep.quiz_results_id_seq', 1, false);
-
-
---
--- Name: users_id_seq; Type: SEQUENCE SET; Schema: bridal_prep; Owner: redu
---
-
-SELECT pg_catalog.setval('bridal_prep.users_id_seq', 1, false);
+ALTER TABLE ONLY public.books
+    ADD CONSTRAINT books_pkey PRIMARY KEY (id);
 
 
 --
--- Name: budgets budgets_pkey; Type: CONSTRAINT; Schema: bridal_prep; Owner: redu
+-- Name: events events_pkey; Type: CONSTRAINT; Schema: public; Owner: redu
 --
 
-ALTER TABLE ONLY bridal_prep.budgets
-    ADD CONSTRAINT budgets_pkey PRIMARY KEY (id);
-
-
---
--- Name: checklist_items checklist_items_pkey; Type: CONSTRAINT; Schema: bridal_prep; Owner: redu
---
-
-ALTER TABLE ONLY bridal_prep.checklist_items
-    ADD CONSTRAINT checklist_items_pkey PRIMARY KEY (id);
-
-
---
--- Name: quiz_results quiz_results_pkey; Type: CONSTRAINT; Schema: bridal_prep; Owner: redu
---
-
-ALTER TABLE ONLY bridal_prep.quiz_results
-    ADD CONSTRAINT quiz_results_pkey PRIMARY KEY (id);
-
-
---
--- Name: users users_email_key; Type: CONSTRAINT; Schema: bridal_prep; Owner: redu
---
-
-ALTER TABLE ONLY bridal_prep.users
-    ADD CONSTRAINT users_email_key UNIQUE (email);
-
-
---
--- Name: users users_pkey; Type: CONSTRAINT; Schema: bridal_prep; Owner: redu
---
-
-ALTER TABLE ONLY bridal_prep.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
-
---
--- Name: budgets budgets_user_id_fkey; Type: FK CONSTRAINT; Schema: bridal_prep; Owner: redu
---
-
-ALTER TABLE ONLY bridal_prep.budgets
-    ADD CONSTRAINT budgets_user_id_fkey FOREIGN KEY (user_id) REFERENCES bridal_prep.users(id) ON DELETE CASCADE;
-
-
---
--- Name: checklist_items checklist_items_user_id_fkey; Type: FK CONSTRAINT; Schema: bridal_prep; Owner: redu
---
-
-ALTER TABLE ONLY bridal_prep.checklist_items
-    ADD CONSTRAINT checklist_items_user_id_fkey FOREIGN KEY (user_id) REFERENCES bridal_prep.users(id) ON DELETE CASCADE;
-
-
---
--- Name: quiz_results quiz_results_user_id_fkey; Type: FK CONSTRAINT; Schema: bridal_prep; Owner: redu
---
-
-ALTER TABLE ONLY bridal_prep.quiz_results
-    ADD CONSTRAINT quiz_results_user_id_fkey FOREIGN KEY (user_id) REFERENCES bridal_prep.users(id) ON DELETE SET NULL;
+ALTER TABLE ONLY public.events
+    ADD CONSTRAINT events_pkey PRIMARY KEY (id);
 
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict bTrkLoLdKCnjvfGgQnMOfACUTipAgOkHbOUfl4QHtnLiEJ20tCCdx0MYPCOoK2a
+\unrestrict kIW3zqcWTcoNw4f75cyYrAgM2sM7Knn6cf9nefVfXfoJXm3lfIQffvqZrfurWgo
 
